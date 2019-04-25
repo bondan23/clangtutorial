4901d6d6df178193ddfb3371852903f57565cea6
Bondan Eko Prasetyo <bondan.ekoprasetyo@gmail.com>
2019-04-25 09:55:25 +0700

    initial commit

--- 2-multi-file/main.c
+++ 2-multi-file/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "calculator.h"
 
-int add(int a, int b) {
-  return -2;
-}
+// int add(int a, int b) {
+//   return -2;
+// }
+
+// clang and nm and libtool
+
+// clang -c calculator.c
+// clang -c main.c
+// clang main.o calculator.o
+
+
+// summary
+// clang file.c -> executable
+// clang -c file.c -> object file | compilation
+// clang a.o b.o c.o | linking
+// static library: libtool -static a.o b.o c.o -o libkuda.a
+// static library: libtool -dynamic a.o b.o c.o -o libkuda.dylib
+// clang -lkuda -L /path/to/libkuda.a x.o y.o
+// clang -I // includea header
 
 int main() {
   printf("30 + 70 = %d\n", add(30, 70));
--- 6-custom-frameworks/main.m
+++ 6-custom-frameworks/main.m
@@ -1,5 +1,7 @@
 #import <CoffeeKit/CoffeeKit.h>
 
+// clang -F ThirdParty/ -framework Foundation main.m
+
 int main() {
   CoffeeMaker *coffeeMaker = [[CoffeeMaker alloc] init];
   [coffeeMaker brew];
